1e7987821d053fe7c3e5600add39b2f60c0dab03
lindongjie <lindj0307@gmail.com>
2015-10-06 15:43:59 +0800

    add
    add project Matchismo

---
+++ Matchismo/Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by 林东杰 on 10/6/15.
+//  Copyright © 2015 Anta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by 林东杰 on 10/6/15.
+//  Copyright © 2015 Anta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
