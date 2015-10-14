6cfeda0fe0338e37e030a1b13923cdcaba3cafa5
lindongjie <lindj0307@gmail.com>
2015-10-14 10:46:45 +0800

    Attributor
    add new project Attributor

---
+++ Attributor/Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by 林东杰 on 10/14/15.
+//  Copyright © 2015 Anta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by 林东杰 on 10/14/15.
+//  Copyright © 2015 Anta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
