6546069dcf1336817bc0abca5f8384564960b50a
lindongjie <lindj0307@gmail.com>
2015-10-15 09:30:20 +0800

    fix attributor's error

--- Attributor/Attributor/ViewController.m
+++ Attributor/Attributor/ViewController.m
@@ -11,19 +11,44 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *header;
 @property (weak, nonatomic) IBOutlet UITextView *body;
+@property (weak, nonatomic) IBOutlet UIButton *outLineButton;
 
 @end
 
 @implementation ViewController
 
-- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
-    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
-}
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString:self.outLineButton.currentTitle];
+    [title setAttributes:@{ NSStrokeWidthAttributeName: @3 ,
+                            NSStrokeColorAttributeName: self.outLineButton.tintColor} range:NSMakeRange(0, [title length])];
+    [self.outLineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    //订阅通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredFontsChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (void)preferredFontsChanged:(NSNotification *) notification {
+    [self usePreferredFonts];
+}
+
+- (void)usePreferredFonts {
+    self.body.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
+    self.header.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self usePreferredFonts];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIContentSizeCategoryDidChangeNotification object:nil];
+}
+
+- (IBAction)changeBodySelectionColorToMatchBackgroundOfButton:(UIButton *)sender {
+    [self.body.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.body.selectedRange];
 }
 
 - (IBAction)outLineBodySelection {
