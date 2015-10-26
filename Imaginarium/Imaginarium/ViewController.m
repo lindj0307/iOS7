6cab73a9cb0720e38a0f9cc649634d005b00182b
lindongjie <lindj0307@gmail.com>
2015-10-26 20:35:59 +0800

    finish imaginarium
    finish imaginarium

--- Imaginarium/Imaginarium/ViewController.m
+++ Imaginarium/Imaginarium/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ImageViewController.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,23 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.destinationViewController isKindOfClass:[ImageViewController class]]) {
+        ImageViewController *ivc = (ImageViewController *)segue.destinationViewController;
+        UIButton *btn =  (UIButton *)sender;
+        ivc.title = [btn currentTitle];
+        if ([segue.identifier isEqualToString: @"picture1"]) {
+            ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://img.netbian.com/file/2015/1019/5e7289765297390113d104161428a341.jpg"]];
+        }
+        if ([segue.identifier isEqualToString: @"picture2"]) {
+            ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://img.netbian.com/file/2015/1024/99e8599090749003137ea46bf1e7b58f.jpg"]];
+        }
+        if ([segue.identifier isEqualToString: @"picture3"]) {
+            ivc.imageURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://img.netbian.com/file/2015/1026/206cb7d2816c2775c38ac83a21b50268.jpg"]];
+        }
+    }
 }
 
 @end
