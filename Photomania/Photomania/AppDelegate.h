b272c90361a3bf6c2ccd57a51b8446f81c497196
lindongjie <lindj0307@gmail.com>
2015-10-30 16:48:41 +0800

    Added new project Photomania

---
+++ Photomania/Photomania/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photomania
+//
+//  Created by 林东杰 on 10/30/15.
+//  Copyright © 2015 Anta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
